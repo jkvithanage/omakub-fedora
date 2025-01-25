e4db9d7a3e6b397e40e2ac34d5a160d9c4febb38
Brendan Scullion <bscullion@proton.me>
2025-01-25 12:38:01 +0000

    use appimage for localsend as no rpm

--- install/desktop/app-localsend.sh
+++ install/desktop/app-localsend.sh
@@ -1,7 +1,8 @@
 # Install LocalSend for local file sharing
 cd /tmp
 LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -O localsend.rpm "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-x86-64.rpm"
-sudo dnf install -y ./localsend.rpm
-rm localsend.rpm
+wget -O localsend.AppImage "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-x86-64.AppImage"
+chmod +x localsend.AppImage
+mkdir -p ~/.local/bin
+mv localsend.AppImage ~/.local/bin/localsend
 cd -
