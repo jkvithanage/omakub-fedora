ea79a0ebcf3dcdfe21861c4a19adb4fc78f2fed7
Brendan Scullion <bscullion@proton.me>
2025-01-25 22:53:55 +0000

    more dnf fixes

--- install/desktop/app-chrome.sh
+++ install/desktop/app-chrome.sh
@@ -1,6 +1,4 @@
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
-cd /tmp
-sudo dnf config-manager --add-repo https://dl.google.com/linux/chrome/rpm/stable/x86_64
 sudo dnf install -y google-chrome-stable
 xdg-settings set default-web-browser google-chrome.desktop
 cd -
--- install/desktop/app-signal.sh
+++ install/desktop/app-signal.sh
@@ -1,4 +1,2 @@
 # Add Signal repository for Fedora
-sudo dnf config-manager --add-repo https://updates.signal.org/desktop/rpm/repodata/repomd.xml
-sudo rpm --import https://updates.signal.org/desktop/rpm/keys.asc
-sudo dnf install -y signal-desktop
+flatpak install -y flathub org.signal.Signal
