61ca739f41bcc54ee967b745c9a54cf4dd6b1cc9
Brendan Scullion <bscullion@proton.me>
2025-01-26 13:50:24 +0000

    fix ghostty config

--- applications/Cursor.sh
+++ applications/Cursor.sh
@@ -2,11 +2,11 @@ cat <<EOF >~/.local/share/applications/Cursor.desktop
 [Desktop Entry]
 Version=1.0
 Name=Cursor
-Comment=
+Comment=Cursor
 Exec=/home/$USER/.local/cursor
 Terminal=false
 Type=Application
 Icon=/home/$USER/.local/share/omakub/applications/icons/Cursor.png
-Categories=Development;
+Categories=GTK;
 StartupNotify=false
 EOF
