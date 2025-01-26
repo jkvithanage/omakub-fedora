09d3b290862ab0a21d641aa6969141b4b82a5826
Brendan Scullion <bscullion@proton.me>
2025-01-26 14:35:03 +0000

    fix cursor

--- applications/Cursor.sh
+++ applications/Cursor.sh
@@ -2,10 +2,12 @@ cat <<EOF >~/.local/share/applications/Cursor.desktop
 [Desktop Entry]
 Version=1.0
 Name=Cursor
+StartupWMClass=cursor
 Comment=Cursor
-Exec=/home/$USER/.local/cursor
+Exec=/home/$USER/.local/bin/cursor.AppImage
 Terminal=false
 Type=Application
+Keywords=Cursor;cursor
 Icon=/home/$USER/.local/share/omakub/applications/icons/Cursor.png
 Categories=GTK;
 StartupNotify=false
